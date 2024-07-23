33df8ada4da9a638a2188e88c2399c0b5cc4787f
Jaachi Okafor <okaforjaachi@gmail.com>
2024-07-23 14:49:39 +0100

    created nav menu for about

---
+++ src/app/about/logo-and-identity/page.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+
+const LogoAndIdentity = () => {
+  return (
+    <div>
+      {/* text writeup */}
+      <div className="ContentContainer">
+        <div className="text-au-100-black flex flex-col gap-5 sm:gap-6 md:gap-[30px] py-10 sm:py-16 md:py-[77px]">
+          <h1 className="text-2xl sm:text-3xl md:text-[40px] leading-tight sm:leading-snug md:leading-[52px] font-light">
+            Logo and Identity
+          </h1>
+          {/* text and image  */}
+          <div className="flex gap-4">
+            {/* text writeup  */}
+            <div>
+              <p className="text-base sm:text-lg md:text-[24px] leading-relaxed sm:leading-loose md:leading-[38px] font-light">
+                The Ashesi mark is a visual representation of university&apos;s
+                mission. The lower part of the logo depicts three pillars that
+                correspond to Ashesi's core values of scholarship, leadership,
+                and citizenship.
+              </p>
+              <p className="text-base sm:text-lg md:text-[24px] leading-relaxed sm:leading-loose md:leading-[38px] font-light">
+                The upper part of the Ashesi mark is an eye, which symbolises
+                exploration and discovery. At a distance, the mark resembles a
+                person standing under a roof, reflecting the importance of
+                community and citizenship to Ashesi&apos;s mission.
+              </p>
+            </div>
+            {/* image */}
+            <div className="w-full h-[30rem] md:h-auto order-1 md:order-2">
+              <div className="relative h-full w-full">
+                <Image
+                  src="/assets/au-web-logo-f-no-bg.png"
+                  fill
+                  alt="About Image preview"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          </div>
+
+          <p className="text-base sm:text-lg md:text-[24px] leading-relaxed sm:leading-loose md:leading-[38px] font-light">
+            Before using the Ashesi logo, please read our{" "}
+            <span className="text-au-burgundy underline cursor-pointer">
+              Branding and Identity Guide.
+            </span>
+          </p>
+
+          {/* Resources */}
+          <div>
+            <h2 className="text-2xl font-bold mb-2">Resources</h2>
+            <ul className="text-2xl font-light list-disc pl-6">
+              <li className="text-au-burgundy underline cursor-pointer">
+                Small JPG image of the Ashesi logo{" "}
+              </li>
+              <li className="text-au-burgundy underline cursor-pointer">
+                Medium sized JPG image of the Ashesi logo{" "}
+              </li>
+              <li className="text-au-burgundy underline cursor-pointer">
+                Large JPG image of the Ashesi logo{" "}
+              </li>
+            </ul>
+          </div>
+
+          {/* business and economics */}
+          <div>
+            <h2 className="text-2xl font-bold mb-2">Website Archives</h2>
+            <ul className="text-2xl font-light list-disc pl-6">
+              <li className="text-au-burgundy underline cursor-pointer">
+                Archive of Ashesi's web sites
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogoAndIdentity;
--- src/constant/Menu.ts
+++ src/constant/Menu.ts
@@ -3,40 +3,67 @@ export const menuData = [
     name: "About",
     link: "/about",
     submenu: [
-      { name: "Overview", link: "/about" },
+      // {name: "Overview", link: "/about"},
       {
-        name: "Facts and Data",
-        link: "/about/facts-and-data/quick-facts",
-        submenu: [
-          { name: "Quick Facts", link: "/about/facts-and-data/quick-facts" },
-          {
-            name: "Mission and History",
-            link: "/about/facts-and-data/mission-and-history",
-          },
-          {
-            name: "University Policies",
-            link: "/about/facts-and-data/university-policies",
-          },
-        ],
+        name: "Vision, Mission and History",
+        link: "/about/vision-mission-and-history",
+      },
+      {
+        name: "Logo and Identity",
+        link: "/about/logo-and-identity",
       },
       {
         name: "Leadership",
-        link: "/about/leadership/governance",
+        link: "/about/leadership/trustees",
         submenu: [
-          { name: "Governance", link: "/about/leadership/governance" },
-          { name: "President's Office", link: "/about/leadership/" },
+          {name: "Trustees", link: "/about/leadership/trustees"},
+          {
+            name: "Governing Council",
+            link: "/about/leadership/governing-council",
+          },
+          {
+            name: "University Management",
+            link: "/about/leadership/university-management",
+          },
+          {
+            name: "Student Leadership",
+            link: "/about/leadership/student-leadership",
+          },
         ],
       },
-      { name: "Our Campus", link: "/about/our-campus" },
-      { name: "Alumni", link: "/about/alumni" },
-      { name: "Contact Us", link: "/about/contact-us" },
+      {name: "Recognition", link: "/about/recognition"},
+      {
+        name: "University Policies",
+        link: "/about/university-policies",
+      },
+      {name: "Reports", link: "/about/reports"},
+      // {
+      //   name: "Facts and Data",
+      //   link: "/about/facts-and-data/quick-facts",
+      //   submenu: [
+      //     {name: "Quick Facts", link: "/about/facts-and-data/quick-facts"},
+      //   ],
+      // },
+      {name: "Our Campus", link: "/about/our-campus"},
+      {name: "Job Openings", link: "/about/job-openings"},
+      {
+        name: "Alumni",
+        link: "/about/alumni",
+        submenu: [
+          {name: "Alumni Relations", link: "/about/alumni/alumni-relations"},
+          {name: "Alumni Council", link: "/about/alumni/alumni-council"},
+          {name: "Alumni Resources", link: "/about/alumni/alumni-resources"},
+          {name: "Make a Gift", link: "/about/alumni/make-a-gift"},
+        ],
+      },
+      {name: "Contact Us", link: "/about/contact-us"},
     ],
   },
   {
     name: "Admission",
     link: "/admission",
     submenu: [
-      { name: "Overview", link: "/admission" },
+      {name: "Overview", link: "/admission"},
       {
         name: "Amadeus Advantage",
         link: "/admission/amadeus-advantage/explore-amadeus",
@@ -59,18 +86,18 @@ export const menuData = [
             name: "Admissions Process",
             link: "/admission/undergraduate-programs/admissions-process",
           },
-          { name: "Fees", link: "/admission/undergraduate-programs/fees" },
+          {name: "Fees", link: "/admission/undergraduate-programs/fees"},
         ],
       },
-      { name: "Our Campus", link: "/admission/our-campus" },
-      { name: "Contact Us", link: "/admission/contact-us" },
+      {name: "Our Campus", link: "/admission/our-campus"},
+      {name: "Contact Us", link: "/admission/contact-us"},
     ],
   },
   {
     name: "Academics",
     link: "/academics",
     submenu: [
-      { name: "Overview", link: "/academics" },
+      {name: "Overview", link: "/academics"},
       {
         name: "Programmes",
         link: "/academics/programmes",
@@ -79,7 +106,7 @@ export const menuData = [
             name: "Overview",
             link: "/academics/programmes",
           },
-          { name: "Humanities", link: "/academics/programmes/humanities" },
+          {name: "Humanities", link: "/academics/programmes/humanities"},
           {
             name: "Economic & Business",
             link: "/academics/programmes/economics-and-business",
@@ -88,17 +115,17 @@ export const menuData = [
             name: "Computer Science",
             link: "/academics/programmes/computer-science",
           },
-          { name: "Engineering", link: "/academics/programmes/engineering" },
+          {name: "Engineering", link: "/academics/programmes/engineering"},
         ],
       },
-      { name: "Learning Goals", link: "#" },
-      { name: "Academic Registry", link: "#" },
-      { name: "Calendar", link: "#" },
+      {name: "Learning Goals", link: "#"},
+      {name: "Academic Registry", link: "#"},
+      {name: "Calendar", link: "#"},
       {
         name: "Library",
         link: "https://library.amadeusuniversity.org/#/app/landing",
       },
-      { name: "Academic Affairs", link: "#" },
+      {name: "Academic Affairs", link: "#"},
     ],
   },
   {
@@ -106,15 +133,15 @@ export const menuData = [
     link: "/student-life",
     submenu: [
       {
-        name: "Student Affairs", 
-        link: "/student-life/student-affairs", 
+        name: "Student Affairs",
+        link: "/student-life/student-affairs",
         submenu: [
           {
             name: "Student Handbook",
             link: "/student-life/student-affairs/student-handbook",
           },
-          { 
-            name: "Honour Code", 
+          {
+            name: "Honour Code",
             link: "/student-life/student-affairs/student-handbook",
           },
           {
@@ -123,26 +150,26 @@ export const menuData = [
           },
         ],
       },
-      { name: "Support", link: "#" },
-      { name: "Custom Development", link: "#" },
+      {name: "Support", link: "#"},
+      {name: "Custom Development", link: "#"},
     ],
   },
   {
     name: "News",
     link: "/news",
     submenu: [
-      { name: "Consulting", link: "#" },
-      { name: "Support", link: "#" },
-      { name: "Custom Development", link: "#" },
+      {name: "Consulting", link: "#"},
+      {name: "Support", link: "#"},
+      {name: "Custom Development", link: "#"},
     ],
   },
   {
     name: "Useful Links",
     link: "/useful-links",
     submenu: [
-      { name: "Consulting", link: "#" },
-      { name: "Support", link: "#" },
-      { name: "Custom Development", link: "#" },
+      {name: "Consulting", link: "#"},
+      {name: "Support", link: "#"},
+      {name: "Custom Development", link: "#"},
     ],
   },
   // { name: 'Job Openings', link: '/job-openings', submenu: [] }
