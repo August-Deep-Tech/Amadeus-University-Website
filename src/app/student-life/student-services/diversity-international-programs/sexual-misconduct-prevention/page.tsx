544ee39b9993cc59aa52bee70af3be941adc43f1
Martins <martins.ikpe@stu.cu.edu.ng>
2024-07-27 01:19:04 +0100

    Fix: Added more pages

--- src/app/student-life/student-services/diversity-international-programs/sexual-misconduct-prevention/page.tsx
+++ src/app/student-life/student-services/diversity-international-programs/sexual-misconduct-prevention/page.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const SexualMisconductPrevention = () => {
   return (
-    <div>SexualMisconductPrevention</div>
+    <div>  {/* text writeup */}
+      <div className="ContentContainer">
+        <div className="text-au-100-black flex flex-col gap-5 sm:gap-6 md:gap-[30px] py-10 sm:py-16 md:py-[77px] text-center">
+          <h1 className="text-2xl sm:text-3xl md:text-[40px] leading-tight sm:leading-snug md:leading-[52px] font-light">
+            Providing a safe campus climate
+          </h1>
+          <p className="text-xl sm:text-lg md:text-[24px] leading-relaxed sm:leading-loose md:leading-[38px] font-light">
+            Ashesi is a zero-tolerance sexual misconduct campus and is committed to providing a safe and conducive environment to learn and engage in meaningful work.  The ODIP ensures that all members of the community are aware of the institutional policies governing all forms of sexual abuse - sexual assault, nonconsensual sexual relationships and harassment - at Ashesi through mandatory Sexual Misconduct Prevention trainings, workshops,
+            and a campus wide awareness and preventive campaigns for all students, staff, and faculty.
+          </p>
+        </div></div>
+    </div>
   )
 }
 
